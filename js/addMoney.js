26435e3e0fdf6b1f13fca01e6072a2736bf635b6
islamrifatislam <islamrifatislam500@gmail.com>
2024-09-20 00:37:12 +0600

    js commit

--- js/addMoney.js
+++ js/addMoney.js
@@ -1,22 +1,22 @@
-// document.getElementById('btn-add-money')
-// .addEventListener('click',function(event){
-// event.preventDefault();
+document.getElementById('btn-add-money')
+.addEventListener('click',function(event){
+event.preventDefault();
 
-// const addMoney=document.getElementById('input-add-money').value;
-// const addMoneyNumber=parseFloat(addMoney);
-// const pinNumber=document.getElementById('input-pin-number').value;
-// console.log(addMoney,pinNumber);
-// // wrong way to varyfay
-// if(pinNumber==='1234'){
-// const balance=document.getElementById('account-balance').innerText;
-// const balanceNumber=parseFloat(balance);
+const addMoney=document.getElementById('input-add-money').value;
+const addMoneyNumber=parseFloat(addMoney);
+const pinNumber=document.getElementById('input-pin-number').value;
+console.log(addMoney,pinNumber);
+// wrong way to varyfay
+if(pinNumber==='1234'){
+const balance=document.getElementById('account-balance').innerText;
+const balanceNumber=parseFloat(balance);
 
-// const newBalance=balanceNumber+addMoneyNumber
-// document.getElementById('account-balance').innerText=newBalance;
-// console.log(balance);
-// }
-// else{
-// alert('Failled to add money')
-// }
+const newBalance=balanceNumber+addMoneyNumber
+document.getElementById('account-balance').innerText=newBalance;
+console.log(balance);
+}
+else{
+alert('Failled to add money')
+}
 
-// })
+})
